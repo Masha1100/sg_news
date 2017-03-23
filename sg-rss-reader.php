5940b8d56dcb07a9d3241d6cc6a3bea6bf327469
Masha1100 <mashka20d@gmail.com>
2017-03-23 13:21:30 +0200

    Added a selection of resources

--- sg-rss-reader.php
+++ sg-rss-reader.php
@@ -3,15 +3,17 @@
 require_once 'vendor/autoload.php';
 require_once 'src/Controller/logger.php';
 
+date_default_timezone_set('Europe/Kiev');
+
 $db = new PDO("mysql:host=localhost;dbname=sg_course;charset=utf8", "root", "123");
 $sql = "INSERT IGNORE INTO sg_news(title, link, description, sourse, pub_date) VALUES( ?, ?, ?, ?, ?)";
 $stmt = $db->prepare($sql);
 
-$feed_urls = [
-    'http://www.pravda.com.ua/rss/',
-    'http://football.ua/rss2.ashx',
-    'https://rss.unian.net/site/news_ukr.rss',
-];
+$sql_urls = "SELECT sourse FROM `news_urls`";
+$stmt_urls = $db->prepare($sql_urls);
+$stmt_urls->execute();
+$feed_urls= $stmt_urls->fetchAll(PDO::FETCH_COLUMN);
+
 
 $feed = new SimplePie();
 $feed->set_feed_url($feed_urls);
@@ -27,12 +29,12 @@ foreach ($items as $item) {
         $item->get_description(),
         $item->get_feed()->get_link(),//обращаемся  к первоисточнику, если больше 1 файла
         //$feed->get_link(),
-        $item->get_date("Y-m-d H:i:s +0200"),
+        $item->get_date("Y-m-d H:i:s"),
        
     ]);
 
     if (! empty($item->get_title()) && ! empty($item->get_link())) {
-         $logger_info->info("Info: writing was succesfull" date("Y-m-d H:i:s"));
+         $logger_info->info("Info: writing was succesfull");
          } else {
              $logger->error("Warning:something wrong");
          }
